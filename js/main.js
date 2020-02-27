f26bc2cf73767dfe90f5469dde0e43ef560b045c
austin <austinkandt@gmail.com>
2020-02-27 18:13:24 -0500

    Cleaned up code

--- js/main.js
+++ js/main.js
@@ -1,49 +1,29 @@
 // Prompt user for vacation type
-var vacationType = prompt("What kind of vacation do you want? Musical, Tropical, or Adventurous?");
-var vacationAnswer = '';
-var destination = '';
+let vacationType = prompt("What kind of vacation do you want? Musical, Tropical, or Adventurous?");
+let destination = '';
 
-if (vacationType === 'Musical') {
-    var vacationAnswer = 'musical';
-    var destination = 'New Orleans';
-
-} else if (vacationType === 'Tropical') {
-    var vacationAnswer = 'tropical';
-    var destination = 'a beach in Mexico'
-
-} else if (vacationType === 'Adventurous') {
-    var vacationAnswer = 'adventurous';
-    var destination = 'whitewater raft in the Grand Canyon'
-
-} else {
-    // This needs to rerun the prompt until they give a viable answer
-    console.log ('haha loser.');
+if (vacationType.toLowerCase() === 'musical') {
+    destination = 'New Orleans';
+} else if (vacationType.toLowerCase() === 'tropical') {
+    destination = 'a beach in Mexico';
+} else if (vacationType.toLowerCase() === 'adventurous') {
+    destination = 'whitewater raft in the Grand Canyon';
 }
 
 
 // Prompt user for group size
-var groupSize = prompt("What is your group size?");
-var groupSizeAnswer = '';
-var travelType = '';
+let groupSize = prompt("What is your group size?");
+let travelType = '';
 
 if (groupSize <= 2) {
-    var groupSizeAnswer = '2';
-    var travelType = 'first class flight';
-
+    travelType = 'first class flight';
 } else if (groupSize <= 5) {
-    var groupSizeAnswer = '5';
-    var travelType = 'helicopter';
-
+    travelType = 'helicopter';
 } else if (groupSize >= 6) {
-    var groupSizeAnswer = 'six or more';
-    var travelType = 'charter flight';
-
-} else {
-    // This needs to rerun the prompt until they give a viable answer
-    console.log ("Take whatever");
+    travelType = 'charter flight';
 }
 
 // Print out the suggested destination and travel method
-let result = (`Since you're a group of ${groupSizeAnswer} going on a ${vacationAnswer} vacation, you should take a ${travelType} to ${destination}.`);
+let result = `Since you're a group of ${groupSize} going on a ${vacationType.toLowerCase()} vacation, you should take a ${travelType} to ${destination}.`;
 
-console.log (result);
+console.log(result);
